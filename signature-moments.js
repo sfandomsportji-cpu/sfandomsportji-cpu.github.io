a675d3a334b8a4bac8aafc6ddaca1e11fb4ab007
sfandomsportji-cpu <sfandomsportji@gmail.com>
2026-09-18 08:58:20 +0900

    Fix persistent global visitor counter
    * fix: persist visitor counter globally with stable 200 baseline
    
    * chore: bust cache for persistent visitor counter

--- signature-moments.js
+++ signature-moments.js
@@ -164,7 +164,7 @@ if(signatureSection&&'IntersectionObserver'in window){
 }
 
 const loadSecondary=()=>{
-  import('./home-editorial.js?v=20260916-v1').catch(()=>{});
+  import('./home-editorial.js?v=20260918-counterv2').catch(()=>{});
 };
 if('requestIdleCallback'in window){requestIdleCallback(loadSecondary,{timeout:1200})}
 else{setTimeout(loadSecondary,350)}
