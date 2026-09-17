44eb64a1dbaf9c5ad61e69a7ebe10cc5a8d70d6d
sfandomsportji-cpu <sfandomsportji@gmail.com>
2026-09-17 09:52:34 +0900

    fix: bust counter loader cache after CounterAPI repair

--- signature-moments.js
+++ signature-moments.js
@@ -171,7 +171,7 @@ if(signatureSection&&'IntersectionObserver'in window){
 }
 
 const loadSecondary=()=>{
-  import('./visitor-counter-bootstrap.js?v=20260916-v1').catch(()=>{});
+  import('./visitor-counter-bootstrap.js?v=20260917-counterrepair1').catch(()=>{});
   import('./home-editorial.js?v=20260916-v1').catch(()=>{});
 };
 if('requestIdleCallback' in window){requestIdleCallback(loadSecondary,{timeout:1200})}
