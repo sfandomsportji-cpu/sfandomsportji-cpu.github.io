4b6578a38f54375e170131751495ca6df6eecc7c
sfandomsportji-cpu <sfandomsportji@gmail.com>
2026-09-17 16:35:05 +0900

    fix: refresh counter loader chain after layout repair

--- signature-moments.js
+++ signature-moments.js
@@ -164,7 +164,7 @@ if(signatureSection&&'IntersectionObserver'in window){
 }
 
 const loadSecondary=()=>{
-  import('./visitor-counter-bootstrap.js?v=20260917-counterrepair2').catch(()=>{});
+  import('./visitor-counter-bootstrap.js?v=20260917-counterrepair3').catch(()=>{});
   import('./home-editorial.js?v=20260916-v1').catch(()=>{});
 };
 if('requestIdleCallback' in window){requestIdleCallback(loadSecondary,{timeout:1200})}
