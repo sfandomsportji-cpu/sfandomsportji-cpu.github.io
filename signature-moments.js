bc9a3b1ca4eebc4f20024ff0b8de89b302076b8e
sfandomsportji-cpu <sfandomsportji@gmail.com>
2026-09-18 09:33:34 +0900

    chore: bust cache for raw counter

--- signature-moments.js
+++ signature-moments.js
@@ -164,7 +164,7 @@ if(signatureSection&&'IntersectionObserver'in window){
 }
 
 const loadSecondary=()=>{
-  import('./home-editorial.js?v=20260918-counterv2').catch(()=>{});
+  import('./home-editorial.js?v=20260918-counterv3').catch(()=>{});
 };
 if('requestIdleCallback'in window){requestIdleCallback(loadSecondary,{timeout:1200})}
 else{setTimeout(loadSecondary,350)}
